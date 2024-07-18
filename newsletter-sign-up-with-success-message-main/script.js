fb6b8544208138c34ea8bf1ef1f1e427e13bc50e
geomydas <155147963+geomydas@users.noreply.github.com>
2024-07-18 09:00:30 +0800

    first commit, added all of the files

---
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -0,0 +1,32 @@
+// I have uploaded the files, do your magic
+
+// List of Classes
+//  1 indent means child
+//  2 indent means child of child (grandchild)
+//  Two line breaks means another completely separate
+
+// newsletter
+
+    // newsletter_text (wrapper)
+    // newsletter_header
+    // newsletter_paragraph
+    // newsletter_list
+    // newsletter_listItem
+    // newsletter_form
+
+        // newsletter_form-label
+        // newsletter_form-email
+        // newsletter_form-submit
+  
+    // newsletter_img
+
+ 
+// success 
+    // success_image
+    // success_header
+    // success_paragraph
+        // success_emphasizetext
+        
+    // success_button    
+
+   
